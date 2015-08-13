c11b38e72fc4ad702655502c41417b5d26dc400a
Chris Campbell <chris@ny-ave.com>
2015-08-13 00:07:39 -0400

    Added Phaser game engine with small demo.

--- client/views/lobby/game.js
+++ client/views/lobby/game.js
@@ -1,35 +1,100 @@
 Template.game.onRendered(function(){
-  var renderer = PIXI.autoDetectRenderer(600, 450,{backgroundColor : 0x1099bb});
-  $("#gameContainer").append(renderer.view);
 
-  // create the root of the scene graph
-  var stage = new PIXI.Container();
+  var game = new Phaser.Game(600, 450, Phaser.AUTO, 'gameContainer', { preload: preload, create: create, update: update });
 
-  // create a texture from an image path
-  var texture = PIXI.Texture.fromImage('bunny.png');
-
-  // create a new Sprite using the texture
-  var bunny = new PIXI.Sprite(texture);
-
-  // center the sprite's anchor point
-  bunny.anchor.x = 0.5;
-  bunny.anchor.y = 0.5;
-
-  // move the sprite to the center of the screen
-  bunny.position.x = 200;
-  bunny.position.y = 150;
-
-  stage.addChild(bunny);
-
-  // start animating
-  animate();
-  function animate() {
-      requestAnimationFrame(animate);
-
-      // just for fun, let's rotate mr rabbit a little
-      bunny.rotation += 0.1;
-
-      // render the container
-      renderer.render(stage);
+  function preload() {
+    game.load.image('sky', 'sky.png');
+    game.load.image('ground', 'platform.png');
+    game.load.image('star', 'star.png');
+    game.load.spritesheet('dude', 'dude.png', 32, 48);
   }
+  var platforms;
+
+  function create() {
+    //  We're going to be using physics, so enable the Arcade Physics system
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    //  A simple background for our game
+    game.add.sprite(0, 0, 'sky');
+
+    //  The platforms group contains the ground and the 2 ledges we can jump on
+    platforms = game.add.group();
+
+    //  We will enable physics for any object that is created in this group
+    platforms.enableBody = true;
+
+    // Here we create the ground.
+    var ground = platforms.create(0, game.world.height - 64, 'ground');
+
+    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+    ground.scale.setTo(2, 2);
+
+    //  This stops it from falling away when you jump on it
+    ground.body.immovable = true;
+
+    //  Now let's create two ledges
+    var ledge = platforms.create(400, 400, 'ground');
+
+    ledge.body.immovable = true;
+
+    ledge = platforms.create(-150, 250, 'ground');
+
+    ledge.body.immovable = true;
+
+    // The player and its settings
+    player = game.add.sprite(32, game.world.height - 150, 'dude');
+
+    //  We need to enable physics on the player
+    game.physics.arcade.enable(player);
+
+    //  Player physics properties. Give the little guy a slight bounce.
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
+
+    //  Our two animations, walking left and right.
+    player.animations.add('left', [0, 1, 2, 3], 10, true);
+    player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+    //  Our controls.
+    cursors = game.input.keyboard.createCursorKeys();
+  }
+
+  function update() {
+    //  Collide the player and the stars with the platforms
+    game.physics.arcade.collide(player, platforms);
+
+    //  Reset the players velocity (movement)
+    player.body.velocity.x = 0;
+
+    if (cursors.left.isDown)
+    {
+        //  Move to the left
+        player.body.velocity.x = -150;
+
+        player.animations.play('left');
+    }
+    else if (cursors.right.isDown)
+    {
+        //  Move to the right
+        player.body.velocity.x = 150;
+
+        player.animations.play('right');
+    }
+    else
+    {
+        //  Stand still
+        player.animations.stop();
+
+        player.frame = 4;
+    }
+
+    //  Allow the player to jump if they are touching the ground.
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.body.velocity.y = -350;
+    }
+  }
+
 });
+1
