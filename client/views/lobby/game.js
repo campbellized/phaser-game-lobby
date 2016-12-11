6dfb92faf249b899a9b41c4ba04384f83ded951e
Chris Campbell <campbellized@gmail.com>
2016-12-11 13:49:10 -0500

    Removed debug info from game camera.

--- client/views/lobby/game.js
+++ client/views/lobby/game.js
@@ -89,7 +89,7 @@ function update() {
 
 function render() {
 
-    game.debug.cameraInfo(game.camera, 32, 32);
+    // game.debug.cameraInfo(game.camera, 32, 32);
     game.debug.spriteCoords(player, 32, 500);
 
 }
