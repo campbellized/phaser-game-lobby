8fd6ab90088f283f38f85a2d3ab6a910331bed00
Chris Campbell <campbellized@gmail.com>
2016-12-14 20:21:12 -0500

    Refactored some Meteor.calls on the server.

--- server/server.js
+++ server/server.js
@@ -71,7 +71,18 @@ Meteor.methods({
     }else{
       console.log("Lobby not found");
     }
-  }
+  },
+  /**
+   * Update the chat history in the Message collection with a user's new alias.
+   * @param  {string} userId  The _id of a document in the User collection
+   * @param  {string} alias The new alias that the user
+   * @return {undefined}
+   */
+  updateChat: function(id, alias){
+      Messages.update({userId: id},
+                      {$set: {userAlias: alias}},
+                      {multi: true});
+  }  
 });
 
 // Events that will be run when a user logs in
